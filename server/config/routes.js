19301a22be2a393f10a7946eecac595216534d0d
al6obasi <maltobasi@hotmail.com>
2017-02-26 18:05:52 +0200

    solved all

--- server/config/routes.js
+++ server/config/routes.js
@@ -7,13 +7,15 @@ module.exports = function (app, express) {
 /*								movie route									 */
 //=============================================================================
 	//add get request for other route
-	app.get('/api/movie/', movieController.getAllMovies);
+	app.get('/api/movie/', movieController.getAllMovies); 
 	app.post('/api/movie/', movieController.insertMovie);
-	
+	app.post('/api/movie/:title', movieController.getMovie); 
 //=============================================================================
 /*								book route									 */
 //=============================================================================
 	//add get and post request for other routes
+	app.get('/api/book/', bookController.getAllBook);
+	app.post('/api/book/', bookController.insertBook);
 	app.get('/api/book/:name', bookController.getByName)
 };
 
--- server/movie/movieController.js
+++ server/movie/movieController.js
@@ -1,4 +1,4 @@
-
+var movie =require('./movieModel.js');
 //check routes.js to see what other functions need to be implemented hint:"you are missing two"
 module.exports ={
 	getMovie : function (req, res) {
@@ -9,5 +9,33 @@ module.exports ={
 				res.json(movie);
 			}
 		}
+	},
+	getAllMovies : function (req, res) {
+		movie.find().exec(function (err, AllMovies) {
+			if(err){
+				res.status(500).send('err');
+			}else{
+				res.status(200).send(AllMovies);
+			}
+		});
+	},
+	insertMovie : function (req, res) {
+			for (var i = 0; i < req.body.length; i++) {
+				var NewMovie = new movie ({
+			        name: req.body[i].name,
+			        director: req.body[i].director,
+			        rate: req.body[i].rate
+				});
+				NewMovie.save(function (err,NewMovie) {
+					console.log("NewMovie")
+					if (err) {
+						res.status(500).res.send(err);
+						console.log(err)
+					}
+				})
+
+			}
+			res.json(req.body);
+			console.log('insert Movie succefuly')
 	}
 }
