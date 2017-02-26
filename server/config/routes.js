a986898cee5ffbee6fb10109bf898a187b280c5b
marwaalalawy <marwa.alalawy@gmail.com>
2017-02-26 18:16:12 +0200

    test passes

--- server/config/routes.js
+++ server/config/routes.js
@@ -8,12 +8,16 @@ module.exports = function (app, express) {
 //=============================================================================
 	//add get request for other route
 	app.get('/api/movie/', movieController.getAllMovies);
+	app.get('/api/movie/:name', movieController.getMovie);
 	app.post('/api/movie/', movieController.insertMovie);
 	
 //=============================================================================
 /*								book route									 */
 //=============================================================================
 	//add get and post request for other routes
-	app.get('/api/book/:name', bookController.getByName)
+	app.get('/api/book/', bookController.getAllBooks);
+	app.get('/api/book/:title', bookController.getBook);
+	app.post('/api/book/', bookController.insertBooks);
+
 };
 
